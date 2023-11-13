04c4f5f941b3585d7c5978ee70b6abdac0624177
streamer45 <cstcld91@gmail.com>
2023-11-13 14:50:49 -0600

    Cache CGO dependencies and Whisper models in Docker build

--- build/build.sh
+++ build/build.sh
@@ -16,7 +16,12 @@ WHISPER_LIBRARY_PATH=${WHISPER_INCLUDE_PATH}
 ONNX_INCLUDE_PATH="/tmp/onnxruntime-linux-x64-${ONNX_VERSION}/include"
 ONNX_LIBRARY_PATH="/tmp/onnxruntime-linux-x64-${ONNX_VERSION}/lib"
 
-bash ./build/prepare_deps.sh ${OPUS_VERSION} ${OPUS_SHA} ${WHISPER_VERSION} ${WHISPER_SHA} "${MODELS}" ${ONNX_VERSION} ${ONNX_SHA} && \
+# Only fetch dependencies if needed (e.g. not already cached by Docker).
+if [ ! -d "$OPUS_INCLUDE_PATH" ]; then
+	echo "Missing dependencies, downloading."
+	bash ./build/prepare_deps.sh ${OPUS_VERSION} ${OPUS_SHA} ${WHISPER_VERSION} ${WHISPER_SHA} "${MODELS}" ${ONNX_VERSION} ${ONNX_SHA}
+fi
+
 C_INCLUDE_PATH="${OPUS_INCLUDE_PATH}:${WHISPER_INCLUDE_PATH}:${ONNX_INCLUDE_PATH}" \
 LIBRARY_PATH="${OPUS_LIBRARY_PATH}:${WHISPER_LIBRARY_PATH}:${ONNX_LIBRARY_PATH}" \
 LD_RUN_PATH="${ONNX_LIBRARY_PATH}" \
