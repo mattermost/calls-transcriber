9ddb10b516cdd9f3d9b1c35e4ae54ae536b95599
Claudio Costa <cstcld91@gmail.com>
2025-06-27 10:39:08 -0600

    Update whisper.cpp to v1.7.5 (#40)

--- build/prepare_deps.sh
+++ build/prepare_deps.sh
@@ -13,10 +13,15 @@ AZURE_SDK_SHA=$9
 IS_BUILD=${10}
 ONNX_ARCH=x64
 ONNX_SHA=a0994512ec1e1debc00c18bfc7a5f16249f6ebd6a6128ff2034464cc380ea211
+CMAKE_VERSION="4.0.3"
+CMAKE_ARCH=x86_64
+CMAKE_SHA=585ae9e013107bc8e7c7c9ce872cbdcbdff569e675b07ef57aacfb88c886faac
 UNAME_M=$(uname -m)
 if [ "$TARGET_ARCH" == "arm64" ]; then
 	ONNX_ARCH=aarch64
+	CMAKE_ARCH=aarch64
 	ONNX_SHA=c1dcd8ab29e8d227d886b6ee415c08aea893956acf98f0758a42a84f27c02851
+	CMAKE_SHA=391da1544ef50ac31300841caaf11db4de3976cdc4468643272e44b3f4644713
 	UNAME_M="arm64"
 fi
 
@@ -25,7 +30,13 @@ if [ "$IS_M1" == "true" ]; then
 	UNAME_M="arm64"
 fi
 
+CMAKE_BASE="cmake-$CMAKE_VERSION-linux-$CMAKE_ARCH"
+CMAKE_PATH="/tmp/$CMAKE_BASE/bin"
+
 cd /tmp && \
+wget "https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/$CMAKE_BASE.tar.gz" && \
+echo "$CMAKE_SHA $CMAKE_BASE.tar.gz" | sha256sum --check && \
+tar xf "$CMAKE_BASE.tar.gz" && \
 wget https://downloads.xiph.org/releases/opus/opus-${OPUS_VERSION}.tar.gz && \
 echo "${OPUS_SHA} opus-${OPUS_VERSION}.tar.gz" | sha256sum --check && \
 tar xf opus-${OPUS_VERSION}.tar.gz && \
@@ -39,7 +50,7 @@ tar xf v${WHISPER_VERSION}.tar.gz && \
 cd whisper.cpp-${WHISPER_VERSION} && \
 ([[ "$TARGET_ARCH" == "amd64" ]] && [[ "$IS_BUILD" == "true" ]] && echo "Patching Whisper.CPP Makefile" && patch -p1 Makefile /src/build/whisper.patch) || true && \
 for model in ${MODELS}; do ./models/download-ggml-model.sh "${model}"; done && \
-make -j4 libwhisper.a UNAME_M=${UNAME_M} && \
+make -j4 UNAME_M=${UNAME_M} PATH="$PATH:$CMAKE_PATH" && \
 cd /tmp && \
 wget https://github.com/microsoft/onnxruntime/releases/download/v${ONNX_VERSION}/onnxruntime-linux-${ONNX_ARCH}-${ONNX_VERSION}.tgz && \
 echo "${ONNX_SHA} onnxruntime-linux-${ONNX_ARCH}-${ONNX_VERSION}.tgz" | sha256sum --check && \
--- cmd/transcriber/apis/whisper.cpp/context_test.go
+++ cmd/transcriber/apis/whisper.cpp/context_test.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -110,7 +111,7 @@ func TestTranscribe(t *testing.T) {
 	segments, lang, err := ctx.Transcribe(samples)
 	require.NoError(t, err)
 	require.NotEmpty(t, segments)
-	require.Equal(t, " This is a test transcription sample.", segments[0].Text)
+	require.Equal(t, "this is a test transcription sample.", strings.ToLower(strings.TrimSpace(segments[0].Text)))
 	require.Equal(t, "en", lang)
 
 	err = ctx.Destroy()
